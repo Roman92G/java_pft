b2f81ab8ff23012b37e958689c6ad2b1da2e52c1
Roman92G <romapeers1992@gmail.com>
2022-03-30 14:42:12 +0300

    вход на сайт перенесен из метода Test в BeforeMethot

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -7,6 +7,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class GroupCreationTests {
     ChromeDriver wd;
 
@@ -14,17 +16,18 @@ public class GroupCreationTests {
     public void setUp() {
         wd = new ChromeDriver();
         wd.manage().window().setSize(new Dimension(1062, 813));
-        //wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-
-    @Test
-    public void testGroupCreation() {
+  //      wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/group.php");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).sendKeys("admin");
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//input[@type='submit']")).click();
+    }
+
+    @Test
+    public void testGroupCreation() {
+
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
@@ -35,12 +38,11 @@ public class GroupCreationTests {
         wd.findElement(By.name("group_footer")).sendKeys("Test3");
         wd.findElement(By.name("submit")).click();
         wd.findElement(By.linkText("group page")).click();
-
-
     }
 
     @AfterMethod
     public void tearDown() {
+
         wd.quit();
     }
 
